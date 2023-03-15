85b9255232ea3dbbcbffb16b0369e2ebe6866e74
MARCOS BAIA <marcosjsbaia@yahoo.com.br>
2023-03-14 23:42:27 -0300

    atualização de código css

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const selecBox = document.querySelector(".select-box"),
 selectXBtn = selecBox.querySelector(".playerX"),
 selectOBtn = selecBox.querySelector(".playerO"),
 playBoard = document.querySelector(".play-board"), 
-allBox = document.querySelectorAll("section span");
+allBox = document.querySelectorAll("section span"),
+player = document.querySelectorAll(".player");
 
 window.onload = ()=> {
+  for (let i = 0; i < allBox.length; i++) {
+    allBox[i].setAttribute("onclick","clickedBox(this)");
+  }
+
   selectXBtn.onclick = ()=>{
     selecBox.classList.add("hide");
     playBoard.classList.add("show");
@@ -12,5 +17,13 @@ window.onload = ()=> {
   selectOBtn.onclick = ()=>{
     selecBox.classList.add("hide");
     playBoard.classList.add("show");
+    player.setAttribute("class", "player active");
   } 
+}
+
+let playerXIcon = "fas fa-times";
+let playerOIcon = "far fa-circle";
+
+function clickedBox(element){
+
 }
